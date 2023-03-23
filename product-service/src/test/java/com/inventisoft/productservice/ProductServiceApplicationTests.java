b3791e7eeed0d00eced08031a840c871df89ba6b
Miguel Lopez <miguel.lopez@avantica.com>
2023-03-23 19:58:09 -0400

    implementacion inicial del microservicio para gestionar productos

---
+++ product-service/src/test/java/com/inventisoft/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventisoft.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
